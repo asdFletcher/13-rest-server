e3440b12d450717c0d74422259ffae08e6f748e0
Fletcher LaRue <laruefletcher@gmail.com>
2019-01-09 21:16:47 -0800

    testing of all in memory routes complete

--- src/models/categories.js
+++ src/models/categories.js
@@ -2,6 +2,7 @@
 
 const uuid = require('uuid/v4');
 const util = require('util');
+const sanitizer = require('./sanitize');
 
 class Categories {
 
@@ -23,16 +24,34 @@ class Categories {
   }
   
   post(record) {
+    record = sanitizer(record);
     record._id = uuid();
     this.database.push(record);
+    return Promise.resolve(record);
   }
 
   put(_id, record) {
-    
+    record = sanitizer(record);
+    for(let i = 0; i < this.database.length; i++){
+      if (this.database[i]._id === _id.toString()){
+        
+        // database match
+        for(let key in record){
+          this.database[i][key] = record[key];
+        }
+        return Promise.resolve(this.database[i]);
+      }
+    }
+    return Promise.resolve({});
   }
 
   delete(_id) {
-
+    for(let i = 0; i < this.database.length; i++){
+      if (this.database[i]._id === _id.toString()){
+        this.database.splice(i,1);
+        return Promise.resolve({});
+      }
+    }
   }
 
 }
